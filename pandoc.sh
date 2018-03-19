d5357917721b7bda54dc2b56bbb4b59af12cf047
damien clochard <damien@dalibo.com>
2018-03-19 12:22:02 +0100

    [wrapper] typo in comment

--- pandoc.sh
+++ pandoc.sh
@@ -7,7 +7,7 @@
 PANDOC_ARGS=("$@")
 # 1. --latex-engine becomes --pdf-engine
 PANDOC_ARGS=("${PANDOC_ARGS[@]//--latex-engine/--pdf-engine}")
-# 2. --no-tex-ligatures becomes --from markdown-smart
+# 2. --no-tex-ligatures becomes --from=markdown-smart
 PANDOC_ARGS=("${PANDOC_ARGS[@]//--no-tex-ligatures/--from=markdown-smart}")
 
 set -x
